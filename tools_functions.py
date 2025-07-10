0cda51df40827949991e31668f09f8cc3b44c83c
Maciej Złotorowicz <maciej.zlotorowicz@fun-crafters.com>
2025-07-10 08:23:38 +0000

    Idea for fixing tools functions and making it easier for LLM to choose them.

--- tools_functions.py
+++ tools_functions.py
@@ -53,6 +53,41 @@ logger = logging.getLogger("Workload LLM Tools")
 # Available Functions Dictionary
 #######################
 
+# TODO Functions to move to be 'proactive'
+# TODO Functions to merge
+# - getChampion()
+#   - db_rag_get_champion_details
+#   - db_get_champion_details
+#   - db_get_champion_details_byid
+#   - if fails, should return - No such champion found. Avalible champions are: <db_get_champions_list()>
+# - getBossInfo()
+#   - db_rag_get_boss_details
+#   - if fails to find anything call other function wih information: No Boss Info found, 
+#   - and should Call RAG databse for more information
+# - searchDatabases()
+#   - db_rag_get_general_knowledge
+#   - db_rag_get_location_details
+#   - db_rag_get_battle_details
+#   - db_get_battle_details
+#   - db_get_battle_details_byid
+# - getBattleDetails()
+#   - db_rag_get_battle_details
+#   - db_get_battle_details
+#   - db_get_battle_details_byid
+# - notKnowHowToName()
+#   - db_get_screen_context_help()
+#   - db_get_ux_details()
+# - compareChampions()
+# - Rest of the tools as they are. Or left for merge later
+# 
+# Tools that should be called when proper embedding found in DB 'proactive' tools.
+# Search after every querry BEFORE LLM generation.
+# - db_rag_get_smalltalk
+# - db_rag_get_champion_details (high treshold) 
+# - db_rag_get_boss_details (high treshold)
+# - db_rag_get_general_knowledge
+# - db_rag_get_gameplay_details
+
 # Available LLM tools for function calling
 available_llm_functions = {
     'db_get_champions_list': {
