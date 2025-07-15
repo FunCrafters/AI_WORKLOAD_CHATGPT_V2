7370a6fa40a3bd93d14e5cbdb748671bdb4eae12
Maciej Złotorowicz <maciej.zlotorowicz@fun-crafters.com>
2025-07-15 12:56:19 +0000

    proper cache

--- tools_functions.py
+++ tools_functions.py
@@ -60,24 +60,22 @@ logger = logging.getLogger("Workload LLM Tools")
 #   - db_get_champion_details
 #   - db_get_champion_details_byid
 #   - if fails, should return - No such champion found. Avalible champions are: <db_get_champions_list()>
-# - getBossInfo()
+# - getBossInfo() # might be merge 
 #   - db_rag_get_boss_details
 #   - if fails to find anything call other function wih information: No Boss Info found, 
 #   - and should Call RAG databse for more information
 # - searchDatabases()
-#   - db_rag_get_general_knowledge
-#   - db_rag_get_location_details
-#   - db_rag_get_battle_details
-#   - db_get_battle_details
-#   - db_get_battle_details_byid
+#   - db_rag_get_general_knowledge # treshold_1
+#   - db_rag_get_location_details # trehdold_2
+#   - if failed to find call <getBattleDetails()> # Might 
 # - getBattleDetails()
 #   - db_rag_get_battle_details
 #   - db_get_battle_details
 #   - db_get_battle_details_byid
-# - notKnowHowToName()
+# - getUIInformation()
 #   - db_get_screen_context_help()
 #   - db_get_ux_details()
-# - compareChampions()
+# - compareChampions() # turn off later
 # - Rest of the tools as they are. Or left for merge later
 # 
 # Tools that should be called when proper embedding found in DB 'proactive' tools.
@@ -86,7 +84,7 @@ logger = logging.getLogger("Workload LLM Tools")
 # - db_rag_get_champion_details (high treshold) 
 # - db_rag_get_boss_details (high treshold)
 # - db_rag_get_general_knowledge
-# - db_rag_get_gameplay_details
+# - db_rag_get_gameplay_details (high treshold)
 
 # Available LLM tools for function calling
 available_llm_functions = {
