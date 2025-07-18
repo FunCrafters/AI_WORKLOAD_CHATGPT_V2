93746f4542a5fe3e1454cefbea19613b46b1c2aa
Maciej Złotorowicz <maciej.zlotorowicz@fun-crafters.com>
2025-07-18 06:37:39 +0000

    bugfix: optimized cosine matrix & fixed logging

--- agents/modules/proactive_smalltalk.py
+++ agents/modules/proactive_smalltalk.py
@@ -1,9 +1,11 @@
+import json
 import logging
 import textwrap
 from typing import List
 
 import numpy as np
 from openai.types.chat import ChatCompletionMessageParam
+from sklearn.metrics.pairwise import cosine_similarity
 
 from agents.modules.module import T3RNModule
 from session import Session
@@ -16,28 +18,24 @@ logger = logging.getLogger("ProactiveSmallTalk")
 class ProactiveSmalltalk(T3RNModule):
     SIMILLARITY_THRESHOLD = 0.5
     VARIATION_THRESHOLD = 0.8
-    INJECT_COUNT = 3
+    INJECT_MAX = 3
 
-    def remove_duplicate(self, smalltalks: List[dict]) -> List[dict]:
-        # calculate each to each similarity matrix
+    def _cosine_matrix(self, smalltalks: List[dict]) -> np.ndarray:
         simmilarity_scores = np.zeros((len(smalltalks), len(smalltalks)))
 
         for i in range(len(smalltalks)):
-            for j in range(len(smalltalks)):
-                if i == j:
-                    continue
-                # calculate cosine similarity
-                simmilarity_scores[i][j] = np.dot(
-                    smalltalks[i]["embedding"], smalltalks[j]["embedding"]
-                ) / (
-                    np.linalg.norm(smalltalks[i]["embedding"])
-                    * np.linalg.norm(smalltalks[j]["embedding"])
+            for j in range(i + 1, len(smalltalks)):
+                simmilarity_scores[i][j] = cosine_similarity(
+                    smalltalks[i]["embedding"],
+                    smalltalks[j]["embedding"],
                 )
+                simmilarity_scores[j][i] = simmilarity_scores[i][j]
 
-        # remove duplicates that fall into the threshold
-        logger.info(f"Simmilarity scores matrix:\n{simmilarity_scores}\n")
+        return simmilarity_scores
 
-        # if two smalltalks are more similar than VARIATION_THRESHOLD, remove one that have lower similarity with original embedding
+    def _remove_duplicate(
+        self, smalltalks: List[dict], simmilarity_scores: np.ndarray
+    ) -> List[dict]:
         to_remove = set()
         for i in range(len(smalltalks)):
             for j in range(len(smalltalks)):
@@ -49,12 +47,22 @@ class ProactiveSmalltalk(T3RNModule):
                     else:
                         to_remove.add(j)
 
-        smalltalks_clean = [s for i, s in enumerate(smalltalks) if i not in to_remove]
+        return [s for i, s in enumerate(smalltalks) if i not in to_remove]
 
-        # remove smalltalks that have similarity below SIMILLARITY_THRESHOLD
+    def _apply_treshold(self, smalltalks: List[dict]) -> List[dict]:
         smalltalks_clean = [
-            s for s in smalltalks_clean if s["similarity"] >= self.SIMILLARITY_THRESHOLD
+            s for s in smalltalks if s["similarity"] >= self.SIMILLARITY_THRESHOLD
         ]
+        return smalltalks_clean
+
+    def remove_duplicate(self, smalltalks: List[dict]) -> List[dict]:
+        simmilarity_scores = self._cosine_matrix(smalltalks)
+
+        logger.debug(f"Simmilarity scores matrix:\n{simmilarity_scores}\n")
+
+        smalltalks_clean = self._remove_duplicate(smalltalks, simmilarity_scores)
+
+        smalltalks_clean = self._apply_treshold(smalltalks_clean)
 
         logger.info(
             f"Removed {len(smalltalks) - len(smalltalks_clean)} duplicates, remaining: {len(smalltalks_clean)}"
@@ -109,7 +117,6 @@ class ProactiveSmalltalk(T3RNModule):
             f"Retrieved smalltalks with similarity: {smalltalks_log} and questions/answers: {qa_log}"
         )
 
-        # concat and sort by similarity
         smalltalks.extend(questions_answers)
         smalltalks = sorted(smalltalks, key=lambda x: x["similarity"], reverse=True)
 
@@ -124,21 +131,19 @@ class ProactiveSmalltalk(T3RNModule):
         ]
 
         self.channel_logger.log_to_tools(
-            f"ProactiveSmallTalk injection (Injecting top {self.INJECT_COUNT}): {smalltalks_clean_log}"
+            f"ProactiveSmallTalk injection (Injecting top {self.INJECT_MAX}): {json.dumps(smalltalks_clean_log, indent=2)}"
         )
 
-        # take atmost 3
         smalltalks_clean = (
-            smalltalks_clean[: self.INJECT_COUNT]
-            if len(smalltalks_clean) > self.INJECT_COUNT
+            smalltalks_clean[: self.INJECT_MAX]
+            if len(smalltalks_clean) > self.INJECT_MAX
             else smalltalks_clean
         )
 
-        # format response for LLM
         response = f"""
-I found some interesting information that might be relevant to this conversation:
+System found some interesting information in your memory banks that might be relevant to this conversation:
 {"\n".join([f"{s['content']} (similarity: {s['similarity']:.2f})" for s in smalltalks_clean])}
-You can use this information to enrich the conversation or ask follow-up questions. Ignore and use tools if not relevant.
+Droid, you can use this to enrich the conversation or ask follow-up questions. Ignore and use tools if not relevant.
 """
 
         injection_messages: List[ChatCompletionMessageParam] = [
